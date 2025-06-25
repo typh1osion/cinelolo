ce7c351679e20a8e6538d813f3d75589c51422c7
James Czerlonka <jamie@pop-os.lan>
2025-06-25 00:06:16 -0400

    Fixed navbar sizing

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,24 +10,30 @@ export default function NavBar() {
         left: 0,
         width: "100%",
         background: "#1f1f1f",
-        display: "flex",
-        justifyContent: "space-between",
+        display: "grid",
+        gridTemplateColumns: "1fr auto 1fr",
         alignItems: "center",
         padding: "0.75rem 1rem",
         zIndex: 1000,
       }}
     >
-      <Link to="/search" title="Search">
-        <FaSearch color="white" size={20} />
-      </Link>
+      <div style={{ justifySelf: "start" }}>
+        <Link to="/search" title="Search">
+          <FaSearch color="white" size={20} />
+        </Link>
+      </div>
 
-      <Link to="/" title="Home">
-        <FaHome color="white" size={20} />
-      </Link>
+      <div style={{ justifySelf: "center" }}>
+        <Link to="/" title="Home">
+          <FaHome color="white" size={20} />
+        </Link>
+      </div>
 
-      <Link to="/favorites" title="Favorites">
-        <FaHeart color="white" size={20} />
-      </Link>
+      <div style={{ justifySelf: "end" }}>
+        <Link to="/favorites" title="Favorites">
+          <FaHeart color="white" size={20} />
+        </Link>
+      </div>
     </nav>
   );
 }
