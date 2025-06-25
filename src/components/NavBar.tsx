687897b12d4c65c9249a49f4cf18e41638ab3a7f
James Czerlonka <jamie@pop-os.lan>
2025-06-25 00:08:57 -0400

    Adjust nav getting cut off

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,30 +10,25 @@ export default function NavBar() {
         left: 0,
         width: "100%",
         background: "#1f1f1f",
-        display: "grid",
-        gridTemplateColumns: "1fr auto 1fr",
+        display: "flex",
+        justifyContent: "space-between",
         alignItems: "center",
-        padding: "0.75rem 1rem",
+        padding: "0.5rem 1rem",
+        boxSizing: "border-box",
         zIndex: 1000,
       }}
     >
-      <div style={{ justifySelf: "start" }}>
-        <Link to="/search" title="Search">
-          <FaSearch color="white" size={20} />
-        </Link>
-      </div>
+      <Link to="/search" title="Search">
+        <FaSearch color="white" size={20} />
+      </Link>
 
-      <div style={{ justifySelf: "center" }}>
-        <Link to="/" title="Home">
-          <FaHome color="white" size={20} />
-        </Link>
-      </div>
+      <Link to="/" title="Home">
+        <FaHome color="white" size={20} />
+      </Link>
 
-      <div style={{ justifySelf: "end" }}>
-        <Link to="/favorites" title="Favorites">
-          <FaHeart color="white" size={20} />
-        </Link>
-      </div>
+      <Link to="/favorites" title="Favorites">
+        <FaHeart color="white" size={20} />
+      </Link>
     </nav>
   );
 }
