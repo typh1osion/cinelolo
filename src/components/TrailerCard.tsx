3f21da2000351ae7afcaabac25804b0194aad20d
James Czerlonka <jamie@pop-os.lan>
2025-06-24 23:51:22 -0400

    adjusted TrailerCard size to be smaller

--- src/components/TrailerCard.tsx
+++ src/components/TrailerCard.tsx
@@ -6,17 +6,17 @@ export default function TrailerCard({ movie }: { movie: any }) {
 
   return (
     <Link to={`/view/${movie.id}`} style={{ textDecoration: "none", color: "white" }}>
-      <div style={{ width: 150 }}>
+      <div style={{ width: 120 }}>
         <img
           src={imageUrl}
           alt={movie.title}
-          style={{ width: "100%", borderRadius: 8 }}
+          style={{ width: "100%", borderRadius: 6 }}
         />
-        <div style={{ marginTop: 8 }}>
-          <p style={{ fontSize: "1rem", fontWeight: "bold", margin: 0 }}>
+        <div style={{ marginTop: 6 }}>
+          <p style={{ fontSize: "0.9rem", fontWeight: "bold", margin: 0 }}>
             {movie.title}
           </p>
-          <p style={{ fontSize: "0.85rem", opacity: 0.8, margin: 0 }}>
+          <p style={{ fontSize: "0.75rem", opacity: 0.8, margin: 0 }}>
             {movie.release_date}
           </p>
         </div>
